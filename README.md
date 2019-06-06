17155333a8078ca9400565e4bad39f959ef02d19
Beloliptsev Aleksey <beloliptsev@kolesa.kz>
2019-06-06 13:53:13 +0600

    Added bytes, latency, status metrics

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ FoundationDB Prometheus exporter
 
 Work in progress. Basic metrics implemented (read/write operations, transaction metrics).
 
-Everything else is missing.
+Added bytes, latency, status metrics.
 
 Dependencies:
 
     pip install prometheus_client foundationdb
 
-Refer to [this document](https://github.com/apple/foundationdb/blob/master/documentation/sphinx/source/mr-status.rst) for details on FoundationDB's metrics and how to access them. 
+Refer to [this document](https://github.com/apple/foundationdb/blob/master/documentation/sphinx/source/mr-status.rst) for details on FoundationDB's metrics and how to access them.
--- fdb_exporter.py
+++ fdb_exporter.py
@@ -28,31 +28,62 @@ class FdbCollector(object):
     def collect(self):
         data = json.loads(read_metrics(self.db).decode())
 
-        # https://github.com/apple/foundationdb/issues/398
-        yield GaugeMetricFamily('fdb_workload_operations_reads_per_second', 
+        yield GaugeMetricFamily('fdb_workload_operations_reads_per_second',
             'Read operations per second',
             value=data['cluster']['workload']['operations']['reads']['hz'])
 
-        yield GaugeMetricFamily('fdb_workload_operations_writes_per_second', 
+        yield GaugeMetricFamily('fdb_workload_operations_writes_per_second',
             'Total number of write operations',
             value=data['cluster']['workload']['operations']['writes']['hz'])
 
-        yield CounterMetricFamily('fdb_workload_operations_writes_total', 
+        yield CounterMetricFamily('fdb_workload_operations_writes_total',
             'Total number of write operations',
             value=data['cluster']['workload']['operations']['writes']['counter'])
 
-        yield CounterMetricFamily('fdb_workload_transactions_committed_total', 
+        yield CounterMetricFamily('fdb_workload_transactions_committed_total',
             'Total number of committed transactions',
             value=data['cluster']['workload']['transactions']['committed']['counter'])
 
-        yield CounterMetricFamily('fdb_workload_transactions_conflicted_total', 
+        yield CounterMetricFamily('fdb_workload_transactions_conflicted_total',
             'Total number of transaction conflicts',
             value=data['cluster']['workload']['transactions']['conflicted']['counter'])
 
-        yield CounterMetricFamily('fdb_workload_transactions_started_total', 
+        yield CounterMetricFamily('fdb_workload_transactions_started_total',
             'Total number of started transactions',
             value=data['cluster']['workload']['transactions']['started']['counter'])
 
+        yield GaugeMetricFamily('fdb_workload_bytes_reads_per_second',
+            'Read bytes per second',
+            value=data['cluster']['workload']['bytes']['read']['hz'])
+
+        yield GaugeMetricFamily('fdb_workload_bytes_writes_per_second',
+            'Total number of writen bytes',
+            value=data['cluster']['workload']['bytes']['written']['hz'])
+
+        yield CounterMetricFamily('fdb_workload_bytes_writes_total',
+            'Total number of writen bytes',
+            value=data['cluster']['workload']['bytes']['written']['counter'])
+
+        yield GaugeMetricFamily('fdb_latency_probe_commit_seconds',
+            'Latency commits seconds',
+            value=data['cluster']['latency_probe']['commit_seconds'])
+
+        yield GaugeMetricFamily('fdb_latency_probe_transaction_start_seconds',
+            'Latency transaction start seconds',
+            value=data['cluster']['latency_probe']['transaction_start_seconds'])
+
+        yield GaugeMetricFamily('fdb_latency_probe_read_seconds',
+            'Latency reads seconds',
+            value=data['cluster']['latency_probe']['read_seconds'])
+
+ 	    yield GaugeMetricFamily('fdb_coordinators_quorum_state', 'Quorum status',
+            value=1 if data['client']['coordinators']['quorum_reachable'] else 0)
+
+        yield GaugeMetricFamily('fdb_database_status_health_state', 'Database health status',
+            value=1 if data['client']['database_status']['healthy'] else 0)
+
+        yield GaugeMetricFamily('fdb_database_status_avail_state', 'Database availability',
+            value=1 if data['client']['database_status']['available'] else 0)
 
 REGISTRY.register(FdbCollector())
 
